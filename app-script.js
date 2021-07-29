608c02f2f8d42d936e0e644e9c4039cd1dda6c4d
Andrew Lloyd <andrew85.lloyd@gmail.com>
2021-07-29 14:53:20 -0700

    Format bytes to MB on google sheets

--- app-script.js
+++ app-script.js
@@ -12,11 +12,23 @@ function doPost(e) {
   if (data.token === getSecrets().token) {
     var date = new Date(data.timestamp * 1000)
     var locale = date.toLocaleString()
-    sheet.appendRow([locale, data.download, data.upload])
+    var downloadMB = formatBytes(data.download)
+    var uploadMB = formatBytes(data.upload)
+    sheet.appendRow([locale, downloadMB, uploadMB])
     returnData.response = "success"
   } else {
     returnData.response = "token not recognized";
   }
-  
+
   return ContentService.createTextOutput(JSON.stringify(returnData))
+}
+
+function formatBytes(bytes, decimals = 2) {
+  if (bytes === 0) return 0
+
+  const k = 1024
+  const dm = decimals < 0 ? 0 : decimals
+  const i = Math.floor(Math.log(bytes) / Math.log(k))
+
+  return parseFloat((bytes / Math.pow(k, i)).toFixed(dm))
 }
